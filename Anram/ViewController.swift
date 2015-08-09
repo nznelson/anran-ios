375331ac9ab9ad4917d374b521caab8a8202b815
Nelson Shaw <nelson.shaw@gmail.com>
2015-08-08 20:55:02 -0700

    Initial Commit

---
+++ Anram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Anram
+//
+//  Created by Nelson Shaw on 8/08/15.
+//  Copyright (c) 2015 Nelson Shaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
